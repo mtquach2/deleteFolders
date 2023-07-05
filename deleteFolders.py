bdf7df93ab0c4b75e893ad6d82897fcb83c48e31
mtquach2 <61102352+mtquach2@users.noreply.github.com>
2023-07-05 11:45:48 -0700

    Update deleteFolders.py

--- deleteFolders.py
+++ deleteFolders.py
@@ -2,7 +2,7 @@ import os
 import shutil 
 import time
 
-def delete_folder(directory):
+def delete_folder(directory, backup_directory):
 	# Get the disk usage of the external drive
 	total, used, free = shutil.disk_usage("E:")
 
@@ -20,11 +20,18 @@ def delete_folder(directory):
 
 			# Check if it is a directory and older than 2 days 
 			if os.path.isdir(subdirectory_path) and current_time - os.path.getctime(subdirectory_path) >= 2 * 24 * 60 * 60:
-				# Delete subdirectory folder 
-				shutil.rmtree(subdirectory_path)
+				
+				# Check if the subdirectory exists in backup directory before removing subdirectory
+				if os.path.exists(os.path.join(backup_directory, subdirectory_path)):
+					
+					# Delete subdirectory 
+					shutil.rmtree(subdirectory_path)
 
 # Specify the directory to check for disk capacity 
-directory_path = "E:/IPG OmniWELD Data" # TODO: Change directory if necessary 
+directory_path = "" # TODO: Change directory if necessary 
+
+# Specify the directory that contains backups
+backup_directory = "" # TODO: Change directory if necessary
 
 # Call the function to delete folders
-delete_folder(directory_path)
+delete_folder(directory_path, backup_directory)
